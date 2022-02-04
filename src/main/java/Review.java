35cf6ce13d7764f49bbfdf8e663b437428765dee
Jing-yan37 <68764963+Jing-yan37@users.noreply.github.com>
2022-02-04 19:54:41 +0800

    Added Review.java class file. (Undo the revert)

---
+++ src/main/java/Review.java
@@ -0,0 +1,46 @@
+public class Review {
+	public Review(int id, int userId, String content, int itemId, String time) {
+		super();
+		Id = id;
+		UserId = userId;
+		Content = content;
+		ItemId = itemId;
+		Time = time;
+	}
+	protected int Id;
+	protected int UserId;
+	protected String Content;
+	protected int ItemId;
+	protected String Time;
+
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public int getUserId() {
+		return UserId;
+	}
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	public int getItemId() {
+		return ItemId;
+	}
+	public void setItemId(int itemId) {
+		ItemId = itemId;
+	}
+	public String getTime() {
+		return Time;
+	}
+	public void setTime(String time) {
+		Time = time;
+	}
+}
