ba2e9ed1bdd81ba024206036637a95a5528279b9
LeftoverRice <nerfmanacostpls@gmail.com>
2022-02-21 11:55:47 +0800

    Able to now test R operation in Selenium

--- src/test/java/AddItemTest.java
+++ src/test/java/AddItemTest.java
@@ -14,7 +14,7 @@ public class AddItemTest {
 	// I want to enter all form fields to create a new item
 	@Test
 	public void fillAddItemForm() {
-		// we need to firstly login first.
+		// we need to firstly login.
 		webDriver.navigate().to("http://localhost:8090/devopsproject/Login.jsp");
 		WebElement email = webDriver.findElement(By.name("email"));
 		WebElement password = webDriver.findElement(By.name("password"));
@@ -45,7 +45,7 @@ public class AddItemTest {
 		itemQuantity.sendKeys("3");
 
 //		try {
-//			Thread.sleep(1000);
+//			Thread.sleep(2000);
 //		} catch (InterruptedException e) {
 //			System.out.println("got interrupted!");
 //		}
@@ -79,7 +79,6 @@ public class AddItemTest {
 	public void afterTest() {
 		// Quit the ChromeDriver and close all associated window at the end of test
 		webDriver.close();
-		webDriver.quit();
 	}
 
 }
